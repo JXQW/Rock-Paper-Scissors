2f80f188c37ab525659249bb63866e24737e750c
JXQW <121765639+JXQW@users.noreply.github.com>
2023-04-15 20:58:01 -0400

    Added Switch Function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let playerInput;
 let cpuInput;
 let numGen;
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 let min = 1;
 let max = 3;
 
@@ -12,12 +12,16 @@ cpuInput = numGen;
 
 function RPS () {
 
+    playerInput = prompt("Please enter rock, paper, or scissors.");
+    switch (playerInput) {
+        case 0:
+            playerInput = "ROCK";
+                if ((playerInput.toUpperCase()= "ROCK") && (cpuInput = "PAPER")) {
+                return "player wins!";
+                playerScore++;
+        break;
+        }
 
-
-    playerInput = prompt("PLease enter rock, paper, or scissors.");
-    if ((playerInput.toUpperCase()= "ROCK") && (cpuInput = "PAPER")) {
-        return "player wins!";
-        playerScore++;
     }
 }
 
