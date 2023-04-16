702ab90e2ee684b969b55a1a82edbd9ef2998f88
JXQW <121765639+JXQW@users.noreply.github.com>
2023-04-15 20:51:18 -0400

    RPS Function added

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ let max = 3;
 
 numGen = Math.floor(Math.random() * (max - min + 1) + min);
 cpuInput = numGen;
+
+function RPS () {
+
+
+
+    playerInput = prompt("PLease enter rock, paper, or scissors.");
+    if ((playerInput.toUpperCase()= "ROCK") && (cpuInput = "PAPER")) {
+        return "player wins!";
+        playerScore++;
+    }
+}
+
+RPS;
