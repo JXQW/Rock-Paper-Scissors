d8ddf801358e7795cdba162b8739900d05137445
JXQW <121765639+JXQW@users.noreply.github.com>
2023-04-15 21:19:13 -0400

    RPS switch function finished

--- index.js
+++ index.js
@@ -6,22 +6,52 @@ let computerScore = 0;
 let min = 1;
 let max = 3;
 
-
-numGen = Math.floor(Math.random() * (max - min + 1) + min);
-cpuInput = numGen;
-
 function RPS () {
+
+    numGen = Math.floor(Math.random() * (max - min + 1) + min);
+    cpuInput = numGen; /* 1 = rock | 2 = paper | 3 = scissors */
 
     playerInput = prompt("Please enter rock, paper, or scissors.");
     switch (playerInput) {
+
         case 0:
             playerInput = "ROCK";
-                if ((playerInput.toUpperCase()= "ROCK") && (cpuInput = "PAPER")) {
+                if (cpuInput = 3) {
                 return "player wins!";
                 playerScore++;
+                } else if (cpuInput = 2) {
+                    return "Computer Wins!"
+                    computerScore++
+                } else {
+                    return "Tie! No points added";
+                }
         break;
-        }
+        
+        case 1:
+            playerInput = "PAPER";
+            if (cpuInput = 1) {
+                return "player wins!";
+                playerScore++;
+                } else if (cpuInput = 3) {
+                    return "Computer Wins!"
+                    computerScore++
+                } else {
+                    return "Tie! No points added";
+                }
+        break;
 
+        case 2:
+            playerInput = "SCISSORS";
+            if (cpuInput = 2) {
+                return "player wins!";
+                playerScore++;
+                } else if (cpuInput = 1) {
+                    return "Computer Wins!"
+                    computerScore++
+                } else {
+                    return "Tie! No points added";
+                }
+        break;
     }
 }
 
