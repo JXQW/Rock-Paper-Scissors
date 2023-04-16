29ef98eb65079d252ac08ec7dca4895a690c3e7d
JXQW <121765639+JXQW@users.noreply.github.com>
2023-04-15 22:36:09 -0400

    added while loop

--- index.js
+++ index.js
@@ -6,53 +6,55 @@ let computerScore = 0;
 let min = 1;
 let max = 3;
 
+console.log("PLayer points " + playerScore + " CPU Points " + computerScore);
+
 function RPS () {
+    while ((playerScore !== 3) || (computerScore !== 3))  {
+        numGen = Math.floor(Math.random() * (max - min + 1) + min);
+        cpuInput = numGen; /* 1 = rock | 2 = paper | 3 = scissors */
 
-    numGen = Math.floor(Math.random() * (max - min + 1) + min);
-    cpuInput = numGen; /* 1 = rock | 2 = paper | 3 = scissors */
+        switch (playerInput) { 
 
-    playerInput = prompt("Please enter rock, paper, or scissors.");
-    switch (playerInput) {
+                case 0:
+                    playerInput = "ROCK";
+                        if (cpuInput = 3) {
+                        return "player wins!"
+                        playerScore++;
+                        } else if (cpuInput = 2) {
+                            return "Computer Wins!"
+                            computerScore++
+                        } else {
+                            return "Tie! No points added";
+                        }
+                break;
+                
+                case 1:
+                    playerInput = "PAPER";
+                    if (cpuInput = 1) {
+                        return "player wins!"
+                        playerScore++;
+                        } else if (cpuInput = 3) {
+                            return "Computer Wins!"
+                            computerScore++
+                        } else {
+                            return "Tie! No points added";
+                        }
+                break;
 
-        case 0:
-            playerInput = "ROCK";
-                if (cpuInput = 3) {
-                return "player wins!";
-                playerScore++;
-                } else if (cpuInput = 2) {
-                    return "Computer Wins!"
-                    computerScore++
-                } else {
-                    return "Tie! No points added";
-                }
-        break;
-        
-        case 1:
-            playerInput = "PAPER";
-            if (cpuInput = 1) {
-                return "player wins!";
-                playerScore++;
-                } else if (cpuInput = 3) {
-                    return "Computer Wins!"
-                    computerScore++
-                } else {
-                    return "Tie! No points added";
-                }
-        break;
-
-        case 2:
-            playerInput = "SCISSORS";
-            if (cpuInput = 2) {
-                return "player wins!";
-                playerScore++;
-                } else if (cpuInput = 1) {
-                    return "Computer Wins!"
-                    computerScore++
-                } else {
-                    return "Tie! No points added";
-                }
-        break;
+                case 2:
+                    playerInput = "SCISSORS";
+                    if (cpuInput = 2) {
+                        return "player wins!"
+                        playerScore++;
+                        } else if (cpuInput = 1) {
+                            return "Computer Wins!"
+                            computerScore++
+                        } else {
+                            return "Tie! No points added";
+                        }
+                break;
+            }
+        }
     }
-}
 
-RPS;
+RPS();
